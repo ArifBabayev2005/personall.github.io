2fe2cfe6a6b2c7c4389963c149ad8b645c1990e0
Arif Babayev <arif.babayev.05@mail.ru>
2021-08-04 11:42:38 +0400

    Finish

---
+++ script.js
@@ -0,0 +1,120 @@
+let menu = document.querySelector('#menu-bars');
+let header = document.querySelector('header');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    header.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    header.classList.remove('active');
+}
+
+let cursor1 = document.querySelector('.cursor-1');
+let cursor2 = document.querySelector('.cursor-2');
+
+window.onmousemove = (e) =>{
+    cursor1.style.top = e.pageY + 'px';
+    cursor1.style.left = e.pageX + 'px';
+    cursor2.style.top = e.pageY + 'px';
+    cursor2.style.left = e.pageX + 'px';
+}
+
+document.querySelectorAll('a').forEach(links =>{
+//My Methods
+var myObj= {
+    //Select TextArea Func
+    textSelect: function(){
+        document.getElementById('description').select();
+    },
+
+//Hide form Method
+    hide: function() {
+    document.getElementById("form").style.display = "none";
+    document.getElementById("show").style.display = "inline-block";
+
+},
+//Show Form Method
+    show:function(){
+
+    document.getElementById("form").style.display = "block";
+    document.getElementById("show").style.display = "none";
+    document.getElementById('myDate').valueAsDate = new Date();
+    },
+    //Removing task method
+    removeTask: function () {
+    var id = this.getAttribute('id');
+    var myTasks = returnToDo();
+    myTasks.splice(id, 1);
+    localStorage.setItem('myData', JSON.stringify(myTasks));
+    document.getElementById('myTasks').innerHTML = '';
+    showMyTasks();
+    console.log('delete');
+
+}
+};
+//Checks if there is already data in LocalStorage
+function returnToDo(){
+    var myTasks = [];
+    var myTasksTemp = localStorage.getItem('myData');
+    if(myTasksTemp != null){
+        myTasks = JSON.parse(myTasksTemp);
+    }
+    return myTasks;
+}
+//Class that creates tasks.
+function Task(){
+    this.name = document.getElementById('name').value;
+    this.subject = document.getElementById('subject').value;
+    this.date = document.getElementById('myDate').value;
+    this.describe = document.getElementById('description').value;
+}
+//Insert task properties into the HTML
+function newTask(x,y,z,o){
+    document.getElementById('myTasks').innerHTML +=
+        '<div class="col l3 m4 s12 animated zoomIn"> <h4>'+z+  ':</h1>'+
+        '<p>'+y+'</p>' +
+        '<p>By: '+x+'</p>' +
+        '<p>Due: ' +o +'</p>'+
+        '<div class="btn red" >Delete</div>'+
+    '</div>'
+}
+//Gets all the objects from the array.
+function showMyTasks(){
+    var myTasks = returnToDo();
+    document.getElementById('myTasks').innerHTML = '';
+    for(var i=0;i<myTasks.length;i++){
+        newTask(
+            myTasks[i].name,
+            myTasks[i].describe,
+            myTasks[i].subject,
+            myTasks[i].date
+        );
+    }
+    var button = document.getElementsByClassName('red');
+    for (var j = 0; j < button.length; j++) {
+        button[j].addEventListener('click', myObj.removeTask);
+        console.log(button[j].addEventListener('click', myObj.removeTask));
+
+    }
+}
+function submitInfo(){
+    var myTasks = returnToDo();
+    myTasks.push(new Task);
+    localStorage.setItem('myData',JSON.stringify(myTasks));
+    showMyTasks();
+    myObj.hide();
+}
+showMyTasks();
+    links.onmouseenter = () =>{
+        cursor1.classList.add('active');
+        cursor2.classList.add('active');
+    }
+
+    links.onmouseleave = () =>{
+        cursor1.classList.remove('active');
+        cursor2.classList.remove('active');
+    }
+
+});
